8dadb760a2ba79c2cd18cec1bc0c5f638fcebefc
Jayita-R <69964059+Jayita-R@users.noreply.github.com>
2021-03-16 22:20:49 +0530

    Add files via upload

---
+++ ObserverPattern/src/observerpattern/MessageSubscriberOne.java
@@ -0,0 +1,11 @@
+package observerpattern;
+
+public class MessageSubscriberOne implements Observer {
+
+	@Override
+	public void update(Message m) {
+		System.out.println("MessageSubscriberOne :: " + m.getMessageContent());
+		
+	}
+
+}
